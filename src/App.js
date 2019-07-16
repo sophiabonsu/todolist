774edab40dfbbb60581651b76a30173c31f66262
sophiabonsu <sophiabonsu@gmail.com>
2019-07-16 17:02:52 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class TodoList extends Component{
+ constructor(props){
+   super(props);
+   this.state ={
+     todos:[],
+     currentToDo:"",
+   };
+     this.addItem = this.addItem.bind(this);
+
+ }
+
+ handleChange = event =>{
+   console.log(event.target.value)
+   this.setState({
+     currentToDo:event.target.value
+
+   })
+ };
+
+ addItem = event =>{
+   event.preventDefault();
+   console.log("addItem Method fired")
+   if (this.state.currentToDo !==""){
+     this.setState({
+       currentToDo:"", 
+       todos:[...this.state.todos,this.state.currentToDo]
+      
+
+     })
+   }
+ };
+
+
+
+
+
+
+
+  render(){
+    return(
+      <div className='container'>
+        <form onSubmit={this.addItem}>
+          <label htmlFor="taskName">Task Name:</label>
+          <input ref={(a) => this._inputElement= a}  onChange={this.handleChange} name="taskName"type="text"placeholder="Add to do here!"/>
+          <button type="submit">Add Task</button>
+        </form>
+       {/* <TodoItems entries={this.state.items}
+                delete={this.deleteItem}/> */}
+      </div>
+    );
+  }
 }
-
-export default App;
+export default TodoList;
