8ede39b42216bfd70fa25acf3a99b0e615bd6bda
sophiabonsu <sophiabonsu@gmail.com>
2019-07-17 16:06:17 -0400

    Updates

---
+++ precache-manifest.1004db276c522805477a945e3c130dc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ffe9fb5bbd506b79f31748b7e12b8a6",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "684258afc4f5912d7a68",
+    "url": "/todolist/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "246f946294448963bb77",
+    "url": "/todolist/static/js/2.10af5e64.chunk.js"
+  },
+  {
+    "revision": "684258afc4f5912d7a68",
+    "url": "/todolist/static/js/main.66d8574d.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
